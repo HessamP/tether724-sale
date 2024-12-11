338741ea787ac70aab628e2146b869a5edfefebf
Hessam <hessam.p@gmail.com>
2024-12-12 00:46:25 +0330

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-xl text-center sm:text-center">
+        Tether724.com is for sale for $1999. For any questions, contact via
+        Telegram: @insightliner
+      </h1>
+    </div>
+  );
+}
